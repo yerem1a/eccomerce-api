5f7cf2c884578c39bf9ff161b12b73d38ea81004
yerem1a <yeremiapurba14@gmail.com>
2024-12-26 11:20:17 +0700

    update authcontroller

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -11,22 +11,22 @@ class AuthController extends Controller
 {
     // Registrasi
     public function register(Request $request)
-    {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:6|confirmed',
-        ]);
+{
+    $validatedData = $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed',
+    ]);
 
-        $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
-            'role' => $request->role ?? 'user',
-        ]);
+    $user = User::create([
+        'name' => $validatedData['name'],
+        'email' => $validatedData['email'],
+        'password' => Hash::make($validatedData['password']),
+    ]);
 
-        return response()->json(['message' => 'User successfully registered'], 201);
-    }
+    return response()->json(['message' => 'User successfully registered'], 201);
+}
+
 
     // Login
     public function login(Request $request)
